77bd3a048093f94e85bde231bf3da316cab5be20
DaviVidal01 <davividal0605@gmail.com>
2023-05-04 16:09:48 -0300

    Entendendo os Operadores do Javascript e como atribui-los nas variáveis do programa

---
+++ 2-Dados_Variáveis_e_Operações/Operadores_Javascript/operadores.js
@@ -0,0 +1,23 @@
+const resultado1 = 1 + 2; // Soma
+const resultado2 = 2 - 2; // Subtração
+const resultado3 = 4 / 2; // Divisão
+const resultado4 = 4 * 2; // Multiplicação
+const resultado5 = 2**3; // Potenciação
+const resultado6 = 29 % 7; // Resto da Divisão (mod)
+let resultado7 = 1; // Incrementar 
+resultado7++;
+let resultado8 = 1; // Decrementar 
+resultado8--;
+
+const text1 = "Dêem follow em "; //Operadores também funcionam com outros tipos de dados
+const text2 = "DaviVidal01 in GitHub";
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
+console.log(resultado6)
+console.log(resultado7)
+console.log(resultado8)
+console.log(text1 + text2)
