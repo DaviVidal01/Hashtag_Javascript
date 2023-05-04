f1d2ed69fbcdf11e5bec0f6c5b27c86f4367b4a3
DaviVidal01 <davividal0605@gmail.com>
2023-05-04 17:59:01 -0300

    Realizando os exercícios do Modulo 2 de JavaScript e finalizando Modulo 2

---
+++ 2-Dados_Variáveis_e_Operações/Exercícios/exercícios.js
@@ -0,0 +1,24 @@
+console.log("Resultado do Exercício 1");
+
+const valor1 = 2148;
+let valor2;
+
+console.log(`O valor da primeira variável é ${valor1}`);
+valor2 =23;
+console.log(`Minha segunda variável vale ${valor2}`);
+console.log(`O valor da soma das minhas duas variáveis é ${valor1 + valor2}`);
+valor2 = valor2**2;
+console.log(`Ao Quadrado: ${valor2}`);
+console.log(`A Divisão: ${valor2/3}`);
+
+console.log('Resultado do exercício 2');
+const bool1 = true;
+const bool2 = false;
+
+console.log(bool1 || bool2);
+console.log(bool1 && bool2);
+
+const bool3 = true;
+
+console.log(bool1 || bool2 || bool3);
+console.log(bool1 && bool2 && bool3);
