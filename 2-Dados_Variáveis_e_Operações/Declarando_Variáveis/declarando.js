af9a26cc2084b53bcf154ee1ca727909e7949bd9
DaviVidal01 <davividal0605@gmail.com>
2023-05-04 15:32:45 -0300

    Estudando definições de variáveis e adicionando arquivo sobre suas regras de nomenclatura

---
+++ 2-Dados_Variáveis_e_Operações/Declarando_Variáveis/declarando.js
@@ -0,0 +1,19 @@
+let variavel0 = "String"; //Variável Let podem ser alterados no código
+const variavel1 = "Follow DaviVidal01"; //Variável Const não podem ser alterados no código
+variavel2 = "Thanks!!"; //Variável sem definição não podem funcionar no código
+
+let _variavel = "Click Stars"; //Variáveis devem ter seu nome sem caracteres especiais, exceto $ e _
+let $variavel = "Click Stars";
+let #variavel = "Click Stars";
+let %variavel = "Click Stars";
+let !variavel = "Click Stars";
+
+let 3variavel = "Coding"; //Variáveis não podem ter números iniciando seu nome, apenas ao longo do nome é permitido
+let variavel3 = "Coding";
+
+let if = "Follow me GitHub"; //Não podemos usar palavras reservadas do JavaScript como nomes de variáveis
+let function = "Follow me GitHub"; 
+
+const cheioDeGraca = "Cheio"; //Em JavaScript é normal respeitarmos o padrão *camelCase* para a nomenclatura de variáveis
+const contabilidadeDeRenda = 100.00;
+const usuárioDeLogin = "DaviVidal01";
