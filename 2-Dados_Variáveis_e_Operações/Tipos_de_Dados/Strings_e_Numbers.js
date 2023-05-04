14e8d22d5d736e3da0c56b0ccb9e4ede0018fb8a
DaviVidal01 <davividal0605@gmail.com>
2023-05-04 17:04:26 -0300

    Entendendo os tipos de Strings e Números, estudando esses tipos de dados

---
+++ 2-Dados_Variáveis_e_Operações/Tipos_de_Dados/Strings_e_Numbers.js
@@ -0,0 +1,13 @@
+const text1 = 'String com Aspas Simples';
+const text2 = "String com Aspas Duplas";
+const text3 = `String com Crase`;
+
+const numeroQueQueroRegistrar = 40028922;
+const stringTemplate = `Registrando o número registrado é ${numeroQueQueroRegistrar}`; //Só funciona com Crase (`)
+console.log(stringTemplate);
+
+const numero1 = 27; // Variável Inteiro
+const numero2 = 23.25; // Variável Float
+const numero3 = -40; // Variável Numero Negativo
+const total = numero1 + numero2 + numero3;
+console.log(total); // total = 10.25
