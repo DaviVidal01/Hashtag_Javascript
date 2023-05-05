16df0c03db4770bc49fc0e62031e05b26d939b0c
DaviVidal01 <davividal0605@gmail.com>
2023-05-05 01:11:11 -0300

    Criando programa "parâmetros_e_argumentos" e entendendo a como utilizar função de outras formas

---
+++ 3-Funções/Parâmetros_Argumentos/parâmetros_e_argumentos.js
@@ -0,0 +1,17 @@
+/*let numeroDaVez = 1;
+console.log(`O número da vez é ${numeroDaVez}`)
+console.log(`O número da vez ${numeroDaVez} ao quadrado vale ${numeroDaVez ** 2}`);
+numeroDaVez = 2;
+console.log(`O número da vez é ${numeroDaVez}`)
+console.log(`O número da vez ${numeroDaVez} ao quadrado vale ${numeroDaVez ** 2}`); */
+
+//-------------------------------------------------------------------------------------
+
+function quadradoDoNumero(numeroDaVez){
+    console.log(`O número da vez é ${numeroDaVez}`)
+    console.log(`O número da vez ${numeroDaVez} ao quadrado vale ${numeroDaVez ** 2}`);
+}
+
+quadradoDoNumero(1);
+quadradoDoNumero(2);
+quadradoDoNumero(3);
