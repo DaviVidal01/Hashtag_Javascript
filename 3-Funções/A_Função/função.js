8fe3b3fdad79b2bfb82bbcbbe58c2d89185bc2d9
DaviVidal01 <davividal0605@gmail.com>
2023-05-05 00:59:11 -0300

    Começando Modulo 3 sobre Funções em Javascript, testando minha primeira função em Javascript

---
+++ 3-Funções/A_Função/função.js
@@ -0,0 +1,15 @@
+/*console.log("Olá boa noite!");
+console.log("Pegue aqui a sua comanda!");
+console.log("Tenha um bom apetite");
+console.log("-------------------------------");
+*/
+// ----------------------------------------------------------
+
+function pegarComanda() {
+    console.log("Olá boa noite!");
+    console.log("Pegue aqui a sua comanda!");
+    console.log("Tenha um bom apetite");
+    console.log("-------------------------------");
+};
+
+pegarComanda;
