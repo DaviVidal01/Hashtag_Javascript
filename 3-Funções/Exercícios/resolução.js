48e27d02596583d1e62ff01e5bc355c56b154deb
DaviVidal01 <davividal0605@gmail.com>
2023-05-06 09:15:33 -0300

    Finalizando o Exercício 2 do Modulo 3

--- 3-Funções/Exercícios/resolução.js
+++ 3-Funções/Exercícios/resolução.js
@@ -1,7 +1,13 @@
 //Resolução do Exercício 1
-function calcularNotas(p1,p2,p3){
-    let media = (p1 + p2 + p3) /3
-    console.log(`A média desse aluno é ${media}`)
+function calcularNotas(p1, p2, p3){
+    const mediaDasNotas = (p1 + p2 + p3) /3;
+    console.log(`A média desse aluno é ${mediaDasNotas}`);
 }
 
-calcularNotas(9,9,9);
+//Resolução do Exercício 2 
+function calcularMedias(mediaDasNotas, p4){
+    mediaFinal = (mediaDasNotas + p4) /2;
+    console.log(`A média final desse aluno é ${mediaFinal}`);
+}
+
+//Resolução do Exercício 3
